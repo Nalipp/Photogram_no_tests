bab856a9594f751859808392c9b9dd462fa44058
Nalipp <nateplipp@gmail.com>
2015-12-30 15:58:50 +0900

    install paperclip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,23 @@
 class PostsController < ApplicationController
-
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @post = Post.create( user_params )
+  end
+
+  private
+    def user_params
+      params.require(:post).permit(:avatar)
+    end
 end
