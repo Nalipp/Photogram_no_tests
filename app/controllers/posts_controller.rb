802655b7e8f905d6cf5ed76a38291c76514f1e1e
Nalipp <nateplipp@gmail.com>
2015-12-30 21:05:44 +0900

    add delete function to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,15 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :show, :destroy]
+
   def index
     @posts = Post.all
   end
 
+  def show
+  end
+
   def new
     @post = Post.new
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def show
-    @post = Post.find(params[:id])
   end
 
   def create
@@ -20,21 +17,32 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path
     else
-      render 'new'
+      render :new
     end
   end
 
-  def update
-  @post = Post.find(params[:id])
-  if @post.update(post_params)
-    redirect_to post_path(@post)
-  else
-    render 'edit'
+  def edit
   end
-end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
 
   private
     def post_params
       params.require(:post).permit(:image, :caption)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
-  has_attached_file :image, styles: { :thumb => "440x", :medium => "580x" }
+  has_attached_file :image, styles: { :thumb => "340x", :medium => "480x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
