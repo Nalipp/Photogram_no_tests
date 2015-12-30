24157e0ec81113a642b48eb6d8385116ad6af80c
Nalipp <nateplipp@gmail.com>
2015-12-30 19:43:33 +0900

    add styling to index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
   def new
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def show
@@ -22,6 +23,16 @@ class PostsController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+  @post = Post.find(params[:id])
+
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
 
   private
     def post_params
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,5 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
-
-  has_attached_file :image, styles: { :thumb => "240x" }
-  has_attached_file :image, styles: { :medium => "340x" }
-  has_attached_file :image, styles: { :large => "440x" }
+  has_attached_file :image, styles: { :thumb => "240x", :medium => "440x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
