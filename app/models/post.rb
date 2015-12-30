a49f6683b5b92857e1e3d542b8746dad2cd3282e
Nalipp <nateplipp@gmail.com>
2015-12-30 20:03:46 +0900

    edit functionality to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
-  has_attached_file :image, styles: { :thumb => "240x", :medium => "440x" }
+  has_attached_file :image, styles: { :thumb => "440x", :medium => "580x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
