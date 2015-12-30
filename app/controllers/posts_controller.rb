89e5311e80427a432aaf84bb8c1f3d748bd8e563
Nalipp <nateplipp@gmail.com>
2015-12-30 16:16:21 +0900

    add create funcitonality to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,14 +10,20 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def create
-    @post = Post.create( user_params )
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   private
-    def user_params
-      params.require(:post).permit(:avatar)
+    def post_params
+      params.require(:post).permit(:image, :caption)
     end
 end
