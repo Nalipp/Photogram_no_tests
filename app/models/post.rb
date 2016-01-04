50c79ac7541e7a9793f605b6c99f47bf6b0b6253
Nalipp <nateplipp@gmail.com>
2016-01-04 18:12:39 +0900

    update  core functionality without git push. opps

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
   validates :image, presence: true
   has_attached_file :image, styles: { :thumb => "340x", :medium => "480x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
